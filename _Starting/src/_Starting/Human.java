b84a3fa084a462dbbcd9c140048ca1e1e2da8161
narekye <yegoryan.narek@gmail.com>
2017-04-23 16:11:21 -0600

    :ok: polimorphism :ok:

---
+++ _Starting/src/_Starting/Human.java
@@ -0,0 +1,16 @@
+package _Starting;
+
+public abstract class Human {
+	private int heigth;
+	private int age;
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public int getHeigth(){
+		return heigth;
+	}
+	
+	public abstract void displayInfo();
+}
---
+++ _Starting/src/_Starting/Worker.java
@@ -0,0 +1,12 @@
+package _Starting;
+
+public class Worker extends Human {
+	private String bank;
+	
+	@Override
+	public void displayInfo() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
